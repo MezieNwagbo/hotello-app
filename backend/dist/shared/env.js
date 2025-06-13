929659aef7170537fb0d62c785c8dc714edc3f6d
Chimezie Nwagbo <chimezienwagbotech@gmail.com>
2025-06-13 16:32:02 +0100

    Remove sensitive and ignored files from version control

--- backend/dist/shared/env.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.FRONTEND_URL = exports.MONGODB_CONNECTION_STRING = exports.JWT_SECRET_KEY = void 0;
-require("dotenv/config");
-exports.JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
-exports.MONGODB_CONNECTION_STRING = process.env.MONGODB_CONNECTION_STRING;
-exports.FRONTEND_URL = process.env.FRONTEND_URL;
