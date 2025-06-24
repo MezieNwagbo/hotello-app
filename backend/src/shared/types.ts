527315a3bad83570f0df6e789598426432a8ac53
Chimezie Nwagbo <chimezienwagbotech@gmail.com>
2025-06-24 06:25:22 +0100

    fixed dist path issue

--- backend/src/shared/types.ts
+++ backend/src/shared/types.ts
@@ -5,3 +5,19 @@ export type AsynFunctionType = (
   res: Response,
   next?: NextFunction
 ) => Promise<any>;
+
+export type HotelType = {
+  _id: string;
+  userId: string;
+  name: string;
+  city: string;
+  country: string;
+  description: string;
+  type: string;
+  adultCount: number;
+  childCount: number;
+  facilities: string[];
+  pricePerNight: number;
+  starRating: number;
+  imageUrls: string[];
+};
